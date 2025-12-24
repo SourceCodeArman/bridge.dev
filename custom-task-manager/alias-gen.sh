f60e289be1be6908743f92d7bbf137addbef5b40
Arman Ghevondyan <58674291+SourceCodeArman@users.noreply.github.com>
2025-12-23 17:42:22 -0800

    Delete custom-task-manager directory

--- custom-task-manager/alias-gen.sh
+++
@@ -1,4 +0,0 @@
-echo "alias t='node custom-task-manager/tasks-cli.js'" >> ~/.bashrc
-echo "alias t='node custom-task-manager/tasks-cli.js'" >> ~/.zshrc
-echo "alias t='node custom-task-manager/tasks-cli.js'" >> ~/.bash_profile
-echo "alias t='node custom-task-manager/tasks-cli.js'" >> ~/.profile
